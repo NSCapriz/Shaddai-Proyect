9da38d840b6da2479240c09787cc6ec3802e0db5
Nicolas Sebastian Capriz <nscapriz@do.scba.gov.ar>
2024-01-08 13:55:26 -0300

    Se agrega contenido al componente Banner y se instala react transition group

--- src/Components/content/Banner.jsx
+++ src/Components/content/Banner.jsx
@@ -1,8 +1,26 @@
+import { useState } from 'react'
 import meditacion from '../../assets/img/meditación.jpg'
+import { SwitchTransition, CSSTransition } from "react-transition-group";
 import '../../assets/css/banner.css'
-import '../../assets/js/consoleText'
+import { useEffect } from 'react';
 
 const Banner = () => {
+    const [parrafoDinamico, setParrafoDinamico] = useState('');
+    const frases = ['Todo lugar es Aquí...', 'Todo momento es Ahora!', 'Un espacio para vos...', 'Un espacio para sanar!'];
+    const [contador, setContador] = useState (0);
+
+    useEffect( ()=> {
+        const interval = setInterval( ()=>{
+            setContador( (currentValue) =>{
+                if (currentValue + 1 === frases.length){
+                    return 0;
+                }
+                return currentValue + 1;
+            }); 
+        }, 3000 );
+        return () => clearInterval(interval);
+    })
+
     return (
         <>
             <div className="particle-canvas">
@@ -11,9 +29,14 @@ const Banner = () => {
                         <img src={meditacion} alt="mujer meditando" className='img-fluid d-flex ' />
                     </div>
                     <div className="col-md-6">
-                        <h1 className='text-center mt-5'>Bienvenidos a Shaddai!</h1>
-                        <p>Nuestro Espacio Holistico.</p>
-                        <div class='console-container'><span id='text'></span><div class='console-underscore' id='console'>&#95;</div></div>
+                        <h1 className='text-center mt-5 ms-2'>Bienvenidos a Shaddai!</h1>
+                        <SwitchTransition>
+                            <CSSTransition classNames="fade" key={frases[contador]} addEndListener={(node, done) => node.addEventListener("transitionend", done, false)}>
+                            <div className='frases ms-2'>{frases[contador]}</div>
+                            </CSSTransition>
+                        </SwitchTransition>
+                        <p className='mt-4 ms-2'>Shaddai, es un espacio Holistico pensado para todas aquellas personas qe buscan armonía, conocimiento interior, desarrollo y crecimiento personal...</p>
+                        <button className='btn btn-primary text-white fw-bolder mt-3 mb-5 ms-2'>Conocer más</button>
                     </div>
                 </div>
             </div>
