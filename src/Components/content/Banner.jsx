e1f4f1be429b6bca57ae94e8823e0424248ba35b
Nicolás Capriz <nicocapriz@gmail.com>
2024-01-08 00:43:35 -0300

    Se agregan animaciones al componente Banner

--- src/Components/content/Banner.jsx
+++ src/Components/content/Banner.jsx
@@ -1,13 +1,23 @@
 import meditacion from '../../assets/img/meditación.jpg'
 import '../../assets/css/banner.css'
+import '../../assets/js/consoleText'
 
 const Banner = () => {
     return (
-        <div className="row align-items-center container-banner">
-            <div className="col-md-6">
-                <img src={meditacion} alt="mujer meditando" className='img-fluid d-flex ' />
+        <>
+            <div className="particle-canvas">
+                <div className="row align-items-center container-banner">
+                    <div className="col-md-6">
+                        <img src={meditacion} alt="mujer meditando" className='img-fluid d-flex ' />
+                    </div>
+                    <div className="col-md-6">
+                        <h1 className='text-center mt-5'>Bienvenidos a Shaddai!</h1>
+                        <p>Nuestro Espacio Holistico.</p>
+                        <div class='console-container'><span id='text'></span><div class='console-underscore' id='console'>&#95;</div></div>
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
