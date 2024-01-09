caaccf853c9f9c457f5aea583185c1c30ef4c426
Nicolás Capriz <nicocapriz@gmail.com>
2024-01-09 00:53:11 -0300

    Se mejoran estilos

--- src/Components/content/Banner.jsx
+++ src/Components/content/Banner.jsx
@@ -7,17 +7,17 @@ import { useEffect } from 'react';
 const Banner = () => {
     const [parrafoDinamico, setParrafoDinamico] = useState('');
     const frases = ['Todo lugar es Aquí...', 'Todo momento es Ahora!', 'Un espacio para vos...', 'Un espacio para sanar!'];
-    const [contador, setContador] = useState (0);
+    const [contador, setContador] = useState(0);
 
-    useEffect( ()=> {
-        const interval = setInterval( ()=>{
-            setContador( (currentValue) =>{
-                if (currentValue + 1 === frases.length){
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setContador((currentValue) => {
+                if (currentValue + 1 === frases.length) {
                     return 0;
                 }
                 return currentValue + 1;
-            }); 
-        }, 3000 );
+            });
+        }, 3000);
         return () => clearInterval(interval);
     })
 
@@ -32,11 +32,13 @@ const Banner = () => {
                         <h1 className='text-center mt-5 ms-2'>Bienvenidos a Shaddai!</h1>
                         <SwitchTransition>
                             <CSSTransition classNames="fade" key={frases[contador]} addEndListener={(node, done) => node.addEventListener("transitionend", done, false)}>
-                            <div className='frases ms-2'>{frases[contador]}</div>
+                                <div className='frases ms-2'>{frases[contador]}</div>
                             </CSSTransition>
                         </SwitchTransition>
                         <p className='mt-4 ms-2'>Shaddai, es un espacio Holistico pensado para todas aquellas personas qe buscan armonía, conocimiento interior, desarrollo y crecimiento personal...</p>
-                        <button className='btn btn-primary text-white fw-bolder mt-3 mb-5 ms-2'>Conocer más</button>
+                        <div className="d-flex justify-content-center align-items-center">
+                            <button className='btn btn-primary text-white fw-bolder mt-3 mb-5 ms-2'>Conocer más</button>
+                        </div>
                     </div>
                 </div>
             </div>
