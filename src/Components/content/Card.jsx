b8ba281d44393ca25cfb586e88f8dccc858044be
Nicolás Capriz <nicocapriz@gmail.com>
2024-01-11 00:15:43 -0300

    Se modifica estructura de componente card

--- src/Components/content/Card.jsx
+++ src/Components/content/Card.jsx
@@ -4,13 +4,14 @@ import '../../assets/css/card.css'
 const Card = () => {
     return (
         <div>
-            <ul>
-                {contenido.map ((contenido, index) => (
+            <ul className="row justify-content-center align-items-center container-list">
+                {contenido.map((contenido, index) => (
                     <li
-                        className={`row  justify-content-center align-items-center my-5 image-container`}
+                        className={`col-md-3 mx-2 px-5 my-5`}
                         key={index}
                     >
-                        <div className="row">
+
+                        <div className="text-center my-3">
                             <div className="d-flex justify-content-center align-items-center">
                                 <img
                                     src={contenido.imagen}
@@ -18,8 +19,6 @@ const Card = () => {
                                     className="image"
                                 />
                             </div>
-                        </div>
-                        <div className="col-md-6 text-center px-5 my-3">
                             <div className="image-description">
                                 <h3>{contenido.titulo}</h3>
                                 <p>{contenido.txt}</p>
