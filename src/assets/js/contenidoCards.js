ece9a211ae622db1ddba7057402821fab0594d86
Nicolas Sebastian Capriz <nscapriz@do.scba.gov.ar>
2024-01-11 13:56:30 -0300

    Se agregan detalles de  los servicios

--- src/assets/js/contenidoCards.js
+++ src/assets/js/contenidoCards.js
@@ -2,17 +2,57 @@ import imagen from '../../assets/img/yoga.png'
 export const contenido = [
     {
         imagen: imagen,
-        titulo: '" Hace mucho más con nostros "',
-        txt: "En Cactus Social Media, entendemos que la clave no es solo crecer, sino florecer. Nuestra experiencia y pasión por la creatividad nos convierten en el socio ideal para hacer que tu marca destaque en el vasto paisaje de las redes sociales.",
+        titulo: 'Reiki Usui',
+        txt: "Es una terapia que trabaja con la energía vital universal. A través de la transmisión de esta energía vital universal conseguimos equilibrar los Chakras, vórtices de energía que se encuentran en distintos puntos de nuestro cuerpo",
     },
     {
         imagen: imagen,
-        titulo: ' "La estrategia es la brújula que guía cada acción "',
-        txt: "En Cactus, cultivamos estrategias que florecen. Somos una agencia que se dedicada a transformar marcas en oasis de éxito. Nuestro equipo de expertos en marketing trabaja en sintonía para crear campañas innovadoras y estrategias que destacan en el árido paisaje de la competencia.",
+        titulo: 'Reiki Energy',
+        txt: "Es una terapia que trabaja con la energía vital universal. A través de la transmisión de esta energía vital universal conseguimos equilibrar los Chakras, vórtices de energía que se encuentran en distintos puntos de nuestro cuerpo",
     },
     {
         imagen: imagen,
-        titulo: ' " Despertá tu potencial "',
-        txt: "Desata el potencial oculto de tu presencia online con nuestro servicio de SEO. En nuestro arsenal de estrategias innovadoras, transformamos tu visibilidad en línea, impulsando tu marca a nuevas alturas. Confiar en nosotros significa desbloquear el camino hacia el éxito digital. ¡Optimiza hoy, destaca siempre!",
+        titulo: 'Flores de Bach',
+        txt: "Es una terapia floral, enmarcada en la medicina natural y que basa su efecto en la acción terapéutica de 38 esencias florales",
+    },
+    {
+        imagen: imagen,
+        titulo: 'Numerología',
+        txt: "Con la numerología podersconocer tu orientación espiritual y energética en función a una carta numerológica y ayudarte a entender tu camino de vida",
+    },
+    {
+        imagen: imagen,
+        titulo: 'Masajes',
+        txt: "Es una terapia basada el uso de distintas técnicas de masaje, para el tratamiento de diversas enfermedades y lesiones, que ayudan disminuir el dolor",
+    },
+    {
+        imagen: imagen,
+        titulo: 'Tarot Zen',
+        txt: "Es una versión del tarot inspirada en la sabiduría de la escuela budista Zen, mostrando constantemente lo que hay aquí y ahora, sin juicio ni comparación para sintonizar con la sensibilidad, la intuición, la receptividad, el coraje y la individualidad.",
+    },
+    {
+        imagen: imagen,
+        titulo: 'Barras access',
+        txt: "Es una terapia manual que consigue, mediante unos suaves toques en ciertos puntos específicos de la cabeza, que las ondas cerebrales cambien, para permitir que la energía fluya por todo el cuerpo, y así liberar tensiones y bloqueos mentales",
+    },
+    {
+        imagen: imagen,
+        titulo: 'Aromaterapia',
+        txt: "Es una terapia para mejorar el bienestar físico, mental y espiritual, basada en el uso de materiales aromáticos, incluidos los aceites esenciales y otros compuestos aromáticos",
+    },
+    {
+        imagen: imagen,
+        titulo: 'Meditación',
+        txt: "Es la técnica por la cual una persona entrena la mente, para enfocar la mente en un objetivo, pensamiento o actividad en particular y darte una sensación de calma, paz y equilibrio",
+    },
+    {
+        imagen: imagen,
+        titulo: 'Cursos y Talleres',
+        txt: "Contamos con variedad de cursos y talleres para que puedas aprender, fórmarte y ser capaz de desarrollar alguna de las terapias o actividades que realizamos en nuestro espacio",
+    },
+    {
+        imagen: imagen,
+        titulo: 'Productos Hólisticos',
+        txt: "Contamos con varios productos hechos con ingredientes naturales que te ayudaran a mantener el equilibrio integral y lograr la armonía de los estados de ánimo",
     },
 ];
