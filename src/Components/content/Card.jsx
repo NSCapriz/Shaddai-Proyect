8cc3ac93a2adb18f9602283e319193844a347a88
Nicolás Capriz <nicocapriz@gmail.com>
2024-01-15 00:36:20 -0300

    Se mejoran estilos cards

--- src/Components/content/Card.jsx
+++ src/Components/content/Card.jsx
@@ -4,10 +4,10 @@ import '../../assets/css/card.css'
 const Card = () => {
     return (
         <div>
-            <ul className="row justify-content-center align-items-center container-list">
+            <ul className="row justify-content-center align-items-center container-list ">
                 {contenido.map((contenido, index) => (
                     <li
-                        className={`col-md-3 mx-2 px-3 my-4`}
+                        className="col-md-3 mx-2 px-3 my-4"
                         key={index}
                     >
 
@@ -15,14 +15,14 @@ const Card = () => {
                             <div className="card-front">
                                 <div className="text-center d-flex justify-content-center align-items-center">
                                     <img
-                                    src={contenido.imagen}
-                                    alt="Imagen 1"
-                                    className="card-image"
+                                        src={contenido.imagen}
+                                        alt={contenido.titulo}
+                                        className="img-fluid card-image"
                                     />
-                                    <h3>{contenido.titulo}</h3>
                                 </div>
+                                <h3 className="text-center mt-5 pt-5 ">{contenido.titulo}</h3>
                             </div>
-                            <div className="card-back">
+                            <div className="card-back pt-5 px-2">
                                 <h3>{contenido.titulo}</h3>
                                 <p>{contenido.txt}</p>
                             </div>
--- src/assets/js/contenidoCards.js
+++ src/assets/js/contenidoCards.js
@@ -1,57 +1,68 @@
-import imagen from '../../assets/img/yoga.png'
+import ReikiUsui from '../../assets/img/reiki.png'
+import ReikiEnergy from '../../assets/img/reiki-energy.png'
+import Flores from '../../assets/img/flores.png'
+import Numerologia from '../../assets/img/numerologia.png'
+import Masajes from '../../assets/img/masaje.png'
+import Tarot from '../../assets/img/tarot.png'
+import Barras from '../../assets/img/masaje-facial.png'
+import Aromaterapia from '../../assets/img/aceite-esencial.png'
+import Meditacion from '../../assets/img/meditacion.png'
+import Cursos from '../../assets/img/taller.png'
+import Productos from '../../assets/img/producto-natural.png'
+
 export const contenido = [
     {
-        imagen: imagen,
+        imagen: ReikiUsui,
         titulo: 'Reiki Usui',
         txt: "Terapia que utiliza la energía vital universal para equilibrar los Chakras, vórtices energéticos en diferentes puntos del cuerpo. A través de la transmisión, se busca armonizar la energía vital para promover el bienestar.",
     },
     {
-        imagen: imagen,
+        imagen: ReikiEnergy,
         titulo: 'Reiki Energy',
         txt: " Es una Terapia  muy especial, que utiliza la energía en diferentes puntos del cuerpo, y que busca armonizar la energía propia para alcanzar la paz interior.",
     },
     {
-        imagen: imagen,
+        imagen: Flores,
         titulo: 'Flores de Bach',
         txt: "Es una terapia floral, enmarcada en la medicina natural y que basa su efecto en la acción terapéutica de 38 esencias florales.",
     },
     {
-        imagen: imagen,
+        imagen: Numerologia,
         titulo: 'Numerología',
         txt: "Con la numerología podersconocer tu orientación espiritual y energética en función a una carta numerológica y ayudarte a entender tu camino de vida.",
     },
     {
-        imagen: imagen,
+        imagen: Masajes,
         titulo: 'Masajes',
         txt: "Es una terapia basada el uso de distintas técnicas de masaje, para el tratamiento de diversas enfermedades y lesiones, que ayudan disminuir el dolor.",
     },
     {
-        imagen: imagen,
+        imagen: Tarot,
         titulo: 'Tarot Zen',
         txt: "Una versión del tarot, inspirada en mostrar constantemente lo que hay aquí y ahora, sin juicio ni comparación a fin de sintonizar  con la sensibilidad, la intuición, la receptividad, el coraje y la individualidad.",
     },
     {
-        imagen: imagen,
+        imagen: Barras,
         titulo: 'Barras access',
         txt: "Terapia manual que, con suaves toques en puntos específicos de la cabeza, modifica las ondas cerebrales para que la energía fluya, liberando tensiones y bloqueos mentales en todo el cuerpo.",
     },
     {
-        imagen: imagen,
+        imagen: Aromaterapia,
         titulo: 'Aromaterapia',
         txt: "Es una terapia para mejorar el bienestar físico, mental y espiritual, basada en el uso de materiales aromáticos, incluidos los aceites esenciales y otros compuestos aromáticos.",
     },
     {
-        imagen: imagen,
+        imagen: Meditacion,
         titulo: 'Meditación',
         txt: "Es la técnica por la cual una persona entrena la mente, para enfocar la mente en un objetivo, pensamiento o actividad en particular y darte una sensación de calma, paz y equilibrio.",
     },
     {
-        imagen: imagen,
+        imagen: Cursos,
         titulo: 'Cursos y Talleres',
         txt: "Contamos con variedad de cursos y talleres para que puedas aprender, fórmarte y ser capaz de desarrollar alguna de las terapias o actividades que realizamos en nuestro espacio.",
     },
     {
-        imagen: imagen,
+        imagen: Productos,
         titulo: 'Productos Hólisticos',
         txt: "Contamos con varios productos hechos con ingredientes naturales que te ayudaran a mantener el equilibrio integral y lograr la armonía de los estados de ánimo.",
     },
